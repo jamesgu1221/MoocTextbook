505f50fb7e3e2e9872744f01b0ae676e9d4e374e
jamesgu1221 <gu_james@sjtu.edu.cn>
2025-09-30 17:16:01 +0800

    Changes to be committed: 	modified:   "Chapter03/3.3 if\350\257\255\345\217\245.cpp" 	new file:   "Chapter03/3.4 switch\350\257\255\345\217\245.cpp" 	modified:   test.cpp

---
+++ Chapter03/3.4 switch语句.cpp
@@ -0,0 +1,25 @@
+//格式
+    /*
+        swtich (表达式){
+        case 整型常量表达式1 : 语句1;
+        case 整型常量表达式2 : 语句2;
+        case 整型常量表达式3 : 语句3;
+
+        default :语句n;     //default可选
+        //从相等的一行起执行到default之前
+        
+    }
+        
+    */
+    
+    /*
+        swtich (表达式){
+        case 整型常量表达式1 : 语句1; break;
+        case 整型常量表达式2 : 语句2; break;
+        case 整型常量表达式3 : 语句3; break;
+
+        default :语句n;     
+        
+    }
+        
+    */
--- test.cpp
+++ test.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 using namespace std;
 
 int main(){
-
-
-    // double x, y;
-    // x = (double)5+6/11;
-    // y = (double)(5+6/11);
-    // cout << x << ' ' << y << endl;
-    // cout <<( 5 > 3 && 2 || 8 < 4);
-
 
     // m >= 10 && m <= 99;
     // a >= 'a' && a <= 'z' || a >= 'A' && a <= 'Z';
     // m % 2 == 1;
     // x != 2 && x != 3;
 
-    
+    // switch(2){
+    //     case 3: cout << "case3" << endl;
+    //     case 2: cout << "case2" << endl;
+    //     case 1: cout << "case1" << endl;
+    //     // case 2: cout << "case2" << endl;
+    //     // case 3: cout << "case3" << endl;
+    //     // case 2: cout << "case2" << endl;
+    // }
+
     return 0;
 
     
