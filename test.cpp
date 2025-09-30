2f0ddbee0b0069ce59b8612e920594e22584cb32
jamesgu1221 <gu_james@sjtu.edu.cn>
2025-09-30 14:37:36 +0800

    modified:   test.cpp

--- test.cpp
+++ test.cpp
@@ -9,7 +9,7 @@ int main(){
     // x = (double)5+6/11;
     // y = (double)(5+6/11);
     // cout << x << ' ' << y << endl;
-    cout <<( 5 > 3 && 2 || 8 < 4);
+    // cout <<( 5 > 3 && 2 || 8 < 4);
 
     // int a;
     
