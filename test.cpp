2088554659386a4b792b1cdd7fb6fd5dcffd4d4e
jamesgu1221 <gu_james@sjtu.edu.cn>
2025-10-09 22:16:36 +0800

    Changes to be committed on 2025/10/9 	modified:   "Chapter03/3.2\351\200\273\350\276\221\350\277\220\347\256\227\347\254\246.cpp" 	modified:   "Chapter03/3.3 if\350\257\255\345\217\245.cpp" 	modified:   "Chapter03/3.4 switch\350\257\255\345\217\245.cpp" 	modified:   test.cpp

--- test.cpp
+++ test.cpp
@@ -10,25 +10,21 @@ int main(){
     // m % 2 == 1;
     // x != 2 && x != 3;
 
-    // switch(2){
-    //     case 3: cout << "case3" << endl;
-    //     case 2: cout << "case2" << endl;
-    //     case 1: cout << "case1" << endl;
-    //     // case 2: cout << "case2" << endl;
-    //     // case 3: cout << "case3" << endl;
-    //     // case 2: cout << "case2" << endl;
+    
+    // double a, b, dlt, integral = 0, y;
+	// cout << "请输入定积分的区间：" ;
+	// cin >> a >> b;
+  	// cout << "请输入小矩形的宽度：" ;
+  	// cin >> dlt;
+
+    // for (double i=a; i<=b; i+=dlt){
+    //     y = i * i + 5 * i + 1;
+    //     integral += dlt * y;
     // }
 
-    double a, b, dlt, integral = 0, y;
-	cout << "请输入定积分的区间：" ;
-	cin >> a >> b;
-  	cout << "请输入小矩形的宽度：" ;
-  	cin >> dlt;
-
-    for (double i=a; i<=b; i+=dlt){
-        y = i * i + 5 * i + 1;
-        integral += dlt * y;
-    }
+    int a=0, b=4, c=5, d;
+    d = a || b+c && b-c;
+    cout << d;
 
 
 
