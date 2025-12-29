01837ca877912166ff58f1f6f5ab0ef995103429
jamesgu1221 <gu_james@sjtu.edu.cn>
2025-12-29 15:11:59 +0800

    Changes to be committed on 2025/12/29 	new file:   "Chapter01\347\273\252\350\256\272/1.1\347\250\213\345\272\217\350\256\276\350\256\241\346\246\202\350\277\260.cpp" 	renamed:    "Chapter01\347\273\252\350\256\272/1.1-1.5.cpp" -> "Chapter01\347\273\252\350\256\272/1.2\350\256\241\347\256\227\346\234\272\347\273\204\346\210\220.cpp" 	new file:   "Chapter01\347\273\252\350\256\272/1.3\347\250\213\345\272\217\350\256\276\350\256\241\350\257\255\350\250\200.cpp" 	new file:   "Chapter01\347\273\252\350\256\272/1.4\347\250\213\345\272\217\350\256\276\350\256\241\350\277\207\347\250\213.cpp" 	modified:   "Chapter02\345\237\272\346\234\254\347\273\204\346\210\220/2.2\345\217\230\351\207\217\344\270\216\345\270\270\351\207\217.cpp" 	modified:   "Chapter05\345\207\275\346\225\260/5.4\345\217\230\351\207\217\347\232\204\345\255\230\345\202\250\347\261\273\345\210\253.cpp" 	modified:   test.cpp

--- test.cpp
+++ test.cpp
@@ -5,24 +5,13 @@
 using namespace std;
 
 
-class A{
-private:
-    int x,y;
-    static int no;
-    int ID;
-public:
-    A(int a=5, int b=10):x(a),y(b){ 
-        ID=++no;
-        cout<<"Object "<<ID<<"(x:"<<x<<" y:"<<y<<") begins:"<<endl;
-    };
-    ~A(){cout<<"Object "<<ID<<" ends:"<<endl;};
-};
-
-int A::no=0;
-
 int main(){
-    A a1(1,2), a2, a3(a1), *p;
-    p=new A();
-    delete p;
-    return 0;
+ 
+    int i;
+    for (i = 0; i < 2; i++) {
+        cout << i;
+
+    }
+
+    cout << 'a';
 }
