a3e7e141ec6b369e69e0f5ec0cdf0f0d7531732b
jamesgu1221 <gu_james@sjtu.edu.cn>
2025-12-30 21:33:37 +0800

    Changes to be committed on 2025/12/30 21:33 	modified:   "Chapter06\346\225\260\347\273\204/6.1\344\270\200\347\273\264\346\225\260\347\273\204.cpp" 	modified:   "Chapter06\346\225\260\347\273\204/6.2\346\237\245\346\211\276.cpp" 	modified:   "Chapter06\346\225\260\347\273\204/6.3\346\216\222\345\272\217.cpp" 	modified:   "Chapter06\346\225\260\347\273\204/6.5\345\255\227\347\254\246\344\270\262.cpp"

--- Chapter06数组/6.2查找.cpp
+++ Chapter06数组/6.2查找.cpp
@@ -1,49 +1,52 @@
-//6.2.1顺序查找
+#include <iostream>
+using namespace std;
+
+//6.2.1 顺序查找
     //for循环
     //最多需要n次
 
-int main(){
-int  k, x;
-int array[] = {2, 3, 1, 7, 5, 8, 9, 0, 4, 6};
-cout << "请输入要查找的元素值："; cin >> x;
-for (k = 0; k < 10; ++k)
-  if (x == array[k]) { 
-    cout << k; 
-    break;
-  }
-if (k == 10) cout << "-1";
-}
+    int main()
+    {
+        int  k, x;
+        int array[] = {2, 3, 1, 7, 5, 8, 9, 0, 4, 6};
+        cout << "请输入要查找的元素值："; 
+        cin >> x;
+        for (k = 0; k < 10; ++k) {
+            if (x == array[k]) { 
+                cout << k; 
+                break;
+            }
+        }
+        if (k == 10) cout << "-1";
+    }
 
-
-//6.2.2二分查找
+//6.2.2 二分查找
     //要求序列已排序
     //最多需要lbn次
 
+    int main()
+    { 
+        int lh, rh, mid, x;
+        int array[ ]={0,1,2,3,4,5,5,6,7,8,9};
+        cout <<"请输入要查找的数据："; 
+        cin >> x;
+        lh = 0, rh = 9;
 
-#include <iostream>
+        while (lh <= rh){
+            mid = (lh + rh)/2;
+            if (array[mid] == x){
+                cout << mid;
+                break;
+            }
+            else if (array[mid] > x){
+                rh = mid-1;         //-1是为了防止陷入死循环
+            }
+            else if (array[mid] < x){
+                lh = mid+1;
+            }
+        }
 
-using namespace std;
+        if (lh > rh) cout << "-1";
 
-int main()
-{ int lh, rh, mid, x;
-  int array[ ]={0,1,2,3,4,5,5,6,7,8,9};
-  cout <<"请输入要查找的数据："; cin >> x;
-  lh = 0, rh = 9;
-
-  while (lh <= rh){
-    mid = (lh + rh)/2;
-    if (array[mid] == x){
-        cout << mid;
-        break;
+        return 0;
     }
-    else if (array[mid] > x){
-        rh = mid-1;
-    }
-    else if (array[mid] < x){
-        lh = mid+1;
-    }
-  }
-if (lh > rh) cout << "-1";
-
-  return 0;
-}
