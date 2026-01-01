129551f2879ffb8afa9b4b55cd0ca4daa2fdd16f
jamesgu1221 <gu_james@sjtu.edu.cn>
2026-01-01 19:26:57 +0800

    Changes to be committed on 2026/1/1 19:27 	modified:   "Chapter07\346\214\207\351\222\210/7.5\346\214\207\351\222\210\344\270\216\345\207\275\346\225\260.cpp" 	modified:   "Chapter07\346\214\207\351\222\210/7.6\345\274\225\347\224\250\347\261\273\345\236\213.cpp"

--- Chapter07指针/7.5指针与函数.cpp
+++ Chapter07指针/7.5指针与函数.cpp
@@ -1,7 +1,33 @@
 //7.5.1 指针作为形式参数
-    //
-    
     //作为函数形参返回多个结果(地址传递)
         //e.g.
             void swap(int *p1, int *p2){};
         //可以通过指针改变实参的值
+
+
+// 7.5.2 数组作为函数参数再讨论
+
+// 7.5.3 字符串作为函数参数
+    //字符串实质上是字符数组，以'\0'结尾
+        //传递字符串只需要传递字符数组的首地址(指向第一个字符的指针)
+        //e.g.
+            void printStr(const char *str){
+                while(*str != '\0'){
+                    cout << *str;
+                    str++;
+                }
+                cout << endl;
+            }
+
+// 7.5.4 返回指针的函数
+    //格式
+        返回类型 *函数名(形参列表){};
+        
+        //e.g.
+            int *createArray(int size){
+                int *array = new int[size];
+                return array;
+            }
+            //需要在不用时释放内存
+                int *arr = createArray(10);
+                delete [] arr;
