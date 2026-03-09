89b8ff29430902c7f996d0c3f643fca6fb2ec36a
jamesgu1221 <gu_james@sjtu.edu.cn>
2026-03-09 16:52:14 +0800

    Changes to be committed: 	modified:   "Chapter08\347\273\223\346\236\204\344\275\223/8.2\351\223\276\350\241\250.cpp"

--- Chapter08结构体/8.2链表.cpp
+++ Chapter08结构体/8.2链表.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 //8.2.1 链表的概念
     //结点：储存一个元素的空间
     //变量head(示例名称)    存放第一个节点的地址
@@ -5,7 +8,7 @@
 //8.2.2 单链表的存储
     //定义结点类型(用结构体)
         struct linkNode {
-            datatype data;
+            int data;
             linkNode* next;
         };   
 
@@ -24,7 +27,7 @@
                 head = s;
         //在指定位置插入
             //在结点p后面插入新结点s
-                LinkNode* s = new LinkNode;
+                linkNode* s = new linkNode;
                 s->data = 20;
                 s->next = p->next;
                 p->next = s;
@@ -33,9 +36,36 @@
         //删除结点p之后的结点
         //为防止p->next == NULL，需先判断
             if(p && p->next){
-                LinkNode* s = p->next;
+                linkNode* s = p->next;
                 p->next = s->next;
                 delete s;
+            }
+
+        //删除第pos个结点
+            int pos;
+            cin >> pos;
+            if (pos < 0) {
+                cout << "位置不合法" << endl;
+                return;
+            } else if (pos ==0) {
+                linkNode *temp = head;
+                head = head->next;
+                delete temp;
+                return;
+            } else {
+                linkNode *current = head;
+                int count = 0;
+                while (current != nullptr && count < pos - 1) {
+                    current = current->next;
+                    count++;
+                }
+                if (current == nullptr || current->next == nullptr) {
+                    cout << "位置不合法" << endl;
+                    return;
+                }
+                linkNode *temp = current->next;
+                current->next = temp->next;
+                delete temp;
             }
 
     //访问结点
@@ -50,6 +80,31 @@
             linkNode *p;
             p = head;
             while(p){
-                if （p->data == x）break;
+                if（p->data == x）break;
                 p = p->next;
             }
+
+    // 反转链表
+        //头插法
+            linkNode *
+
+        //迭代法
+            linkNode *prev = nullptr;
+            linkNode *current = head;
+
+//8.2.4 单循环链表
+
+//8.2.5 双向链表
+    //插入结点
+        //在结点p后面插入新结点s
+            linkNode* s = new linkNode;
+            cin >> s->data;
+            s -> next = p -> next;
+            s -> prev = p;
+            p -> next = s;
+            p -> next -> prev = s;
+
+
+
+//8.2.6 广义表
+    //定义广义表的结点类型
